166515d4c8794f47fecfbdf74cd3bebcf61f2fbd
Christine Cheng <ccheng18@myth52.stanford.edu>
2021-11-30 00:23:16 -0800

    new todo's

--- asst4/bfs/bfs.cpp
+++ asst4/bfs/bfs.cpp
@@ -75,14 +75,13 @@ void bfs_top_down(Graph graph, solution* sol) {
     // omp get thread num for size of vertex set. numthreads ver
     vertex_set** new_frontier_array = new vertex_set*[omp_get_max_threads()];
     //maybe have to max threads* max nodes so that we dont have to do 32 mallocs
+    // KEV TODO: reduction for all the new frontiers? we are essentially letting each thread have it's own new frontier then putting it all together (consolidate) into a single new frontier) after all the parallel work is done
+
     for (int i = 0; i < omp_get_max_threads(); i++) {
         new_frontier_array[i] = new vertex_set();
+        vertex_set_init(new_frontier_array[i], graph->num_nodes);
+
     }
-    //new_frontier_arrayp[i] = new vertex_set();
-    //vertex_set_init(&list1, graph->num_nodes);
-
-    //for (each element)
-
     // initialize all nodes to NOT_VISITED
     for (int i=0; i<graph->num_nodes; i++) {
         sol->distances[i] = NOT_VISITED_MARKER;
@@ -98,7 +97,7 @@ void bfs_top_down(Graph graph, solution* sol) {
         double start_time = CycleTimer::currentSeconds();
 #endif
 
-        vertex_set_clear(new_frontier);
+        // vertex_set_clear(new_frontier);
 
         top_down_step(graph, frontier, new_frontier_array, sol->distances);
 
@@ -108,6 +107,12 @@ void bfs_top_down(Graph graph, solution* sol) {
 #endif
     // TODO: how to put all the new frontiers into one frontier
     // for loop of membpy into the new frontier for the count into vertex list.
+    // KEV TODO: somehow use reduction to put the new frontiers together into one actual new frontier.
+        for (int i = 0; i < omp_get_max_threads(); i++) {
+            memcpy(new_frontier + new_frontier->count, new_frontier_array[i], new_frontier_array[i]->count);
+            new_frontier->count += new_frontier_array[i]->count;
+            vertex_set_clear(new_frontier_array[i]);
+        }
 
         // swap pointers
         vertex_set* tmp = frontier;
@@ -123,7 +128,7 @@ void bottom_up_step(
     int* distances)
 {
 #pragma omp parallel for schedule(dynamic, 1000)
-
+    // KEV TODO: somehow do reduction stuffs on the new frontier???
     for (int i=0; i<g->num_nodes; i++) {
     // if already in, then break TODO: how to break in parallel for loop? is it just break or continue?
         if (distances[i] == NOT_VISITED_MARKER) {
@@ -137,7 +142,7 @@ void bottom_up_step(
                     // If in frontier
                     if (frontier->vertices[*v] == 1) {
                         // This node is in new frontier
-                        new_frontier->vertices[i] = 1;
+                        new_frontier->vertices[new_frontier->count] = i;
                         // Set distance as v's distance + 1 TODO: need CAS here?
                         distances[i] = distances[*v] + 1;
                         // Increment the new frontier counter atomically
@@ -182,7 +187,6 @@ void bfs_bottom_up(Graph graph, solution* sol)
 
     vertex_set* frontier = &list1;
     vertex_set* new_frontier = &list2;
-
     // initialize all nodes to NOT_VISITED
     for (int i=0; i<graph->num_nodes; i++) {
         sol->distances[i] = NOT_VISITED_MARKER;
@@ -197,6 +201,11 @@ void bfs_bottom_up(Graph graph, solution* sol)
 
         vertex_set_clear(new_frontier); // TODO: does this set things to 0 again?
         bottom_up_step(graph, frontier, new_frontier, sol->distances);
+        // TODO: ask if it's better to for loop like this to make frontier hold index's
+        for (int i = 0; i < new_frontier->count) {
+            // KEV TODO: Make frontier into new frontier. new frontier currently holds index's of which nodes should be in frontier. frontier is a flags list: frontier[index] = 1 means node index is in frontier
+            // We do this so that the switch thingy will be easier. We could also implement this such that frontier holds index's rather than flags (this would be best for switch), but Idk if this is better for being fast
+        }
 
          // swap pointers
         vertex_set* tmp = frontier;
@@ -248,13 +257,17 @@ void bfs_hybrid(Graph graph, solution* sol)
         vertex_set_clear(new_frontier);
 
         if (frontier->count > num_unvisited_nodes) {
+            for (int i = 0; i < frontier->count; i++) {
+
+            }
+
             bottom_up_step(graph, frontier, new_frontier, sol->distances);
 
             //num nodes visited vs num nodes in frontier
         } else {
             // TODO: Set up top down reduction stuffs
 
-            top_down_step(graph, frontier, &new_frontier, sol->distances);
+            //top_down_step(graph, frontier, &new_frontier, sol->distances);
             // TODO: Put together new frontier from array of new frontiers
         }
 
