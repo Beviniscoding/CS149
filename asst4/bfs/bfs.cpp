1ebf3d272ff32c8a00f6b181ffd29342f2fdf2d0
Ubuntu <ubuntu@ip-172-31-4-65.us-east-2.compute.internal>
2021-12-02 23:18:48 +0000

    69.90

--- asst4/bfs/bfs.cpp
+++ asst4/bfs/bfs.cpp
@@ -189,9 +189,9 @@ void bfs_bottom_up(Graph graph, solution* sol)
         converged = 0;
 
         memset(new_frontiers, 0, sizeof(bool) * numNodes);
-        //int* new_frontiers = new int[numNodes];
+        
         bottom_up_step(graph, sol->distances, indic_frontier, new_frontiers,converged);
-        //memcpy(indic_frontier,new_frontiers,numNodes*sizeof(int));
+        
         //delete(new_frontiers);
         // swap pointers
         bool* tmp = indic_frontier;
@@ -217,10 +217,26 @@ void bfs_bottom_up(Graph graph, solution* sol)
 
 void bfs_hybrid(Graph graph, solution* sol)
 {
-    bfs_top_down(graph,sol);
-    /*
-    int num_unvisited_nodes = graph->num_nodes;
+    //bfs_top_down(graph,sol);
 
+    //set up for bottom up
+
+    int numNodes = graph -> num_nodes;
+   
+    bool* indic_frontier = new bool[numNodes];//might have to use new with other arrays which may be iffy 
+    memset(indic_frontier, 0, sizeof(bool) * numNodes);
+    // setup frontier flags with first as 1
+    indic_frontier[ROOT_NODE_ID] = 1;
+    
+    int converged = 1;
+    bool* new_frontiers = new bool[numNodes];
+    memset(new_frontiers, 0, sizeof(bool) * numNodes);
+
+    bool lastWasBottom = 0;
+
+
+    // set up for top down
+    int num_unvisited_nodes = numNodes;
     vertex_set list1;
     vertex_set list2;
     vertex_set_init(&list1, graph->num_nodes);
@@ -228,6 +244,18 @@ void bfs_hybrid(Graph graph, solution* sol)
 
     vertex_set* frontier = &list1;
     vertex_set* new_frontier = &list2;
+
+    // TODO: reduction for all new frontiers
+
+   // vertex_set** new_frontier_array = (vertex_set**)malloc(sizeof(vertex_set*) * omp_get_max_threads());
+    //std::cout << "omp threads: " << omp_get_max_threads() << std::endl;
+    vertex_set** new_frontier_array = new vertex_set*[omp_get_max_threads()];
+
+    for (int i = 0; i < omp_get_max_threads(); i++)  {
+        new_frontier_array[i] = new vertex_set();
+      //  std::cout << "thread index: " << i << std::endl;
+        vertex_set_init(new_frontier_array[i], graph->num_nodes);
+    }
 
     // initialize all nodes to NOT_VISITED
     for (int i=0; i<graph->num_nodes; i++) {
@@ -241,24 +269,56 @@ void bfs_hybrid(Graph graph, solution* sol)
     while (frontier->count != 0) {
         num_unvisited_nodes -= frontier->count;
         vertex_set_clear(new_frontier);
+        
         if (frontier->count > num_unvisited_nodes) {
-            //for (int i = 0; i < frontier->count; i++) {
-            //}
+            //std::cout << "going into bottom up" << std::endl;
+            if (!lastWasBottom) {
+                // last time was top down, must readjust stuffs
+                for (int i = 0; i < frontier -> count; i++){
+                    indic_frontier[frontier->vertices[i]] = true;
+                    
+                }
+            }
+            // do bottom up step
+            lastWasBottom = 1;
+            frontier -> count = 0;
+            memset(new_frontiers, 0, sizeof(bool) * numNodes);
+            bottom_up_step(graph, sol->distances, indic_frontier, new_frontiers,frontier -> count);
+            // swap pointers
+            bool* tmp = indic_frontier;
+            indic_frontier = new_frontiers;
+            new_frontiers = tmp;
 
-            bottom_up_step(graph, sol->distances,frontier -> vertices,new_frontier -> vertices,frontier -> count);
-            //num nodes visited vs num nodes in frontier
         } else {
-            // TODO: Set up top down reduction stuffs
+           // std::cout << "going into top down" << std::endl;
+            if (lastWasBottom) {
+                // last time was bottom up, need to adjust for top down
+                int num_input = 0;
+                   for (int i = 0; i < numNodes; i++){
+                    if (indic_frontier[i]){
+                        frontier->vertices[num_input] = i;
+                        num_input++;
+                    }
+                }
+            }
+           // std::cout << "finish set up top down step" << std::endl;
+            lastWasBottom = 0;
+        vertex_set_clear(new_frontier);
+        top_down_step(graph, frontier, new_frontier_array, sol->distances);
 
-            top_down_step(graph, frontier, &new_frontier, sol->distances);
-            // TODO: Put together new frontier from array of new frontiers
+    // TODO: how to put all the new frontiers into one frontier
+        for (int i = 0; i < omp_get_max_threads(); i++) {
+            memcpy(new_frontier -> vertices + new_frontier -> count , new_frontier_array[i] -> vertices, new_frontier_array[i] -> count * sizeof(int));
+            new_frontier->count += new_frontier_array[i] -> count;
+            vertex_set_clear(new_frontier_array[i]);
         }
         // swap pointers
         vertex_set* tmp = frontier;
         frontier = new_frontier;
         new_frontier = tmp;
+        }
     }
-    */
+    
     // CS149 students:
     //
     // You will need to implement the "hybrid" BFS here as
